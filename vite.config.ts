9140b3463126a94fe97a7d89032db9c418e0d016
Pietro Rossi <pietro.rossi@bbw.ch>
2026-02-03 07:38:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/srg-initiative/',
     plugins: [react()],
     resolve: {
         alias: {
